e27687b4f117b33f76bfd83da00fd3b29bb678eb
emilytian0 <emilytian@ucsb.edu>
2024-04-08 17:17:34 -0700

    Co-authored-by: christy <qwistaycat@users.noreply.github.com>

--- frontend/refract/src/components/MobileUI.tsx
+++ frontend/refract/src/components/MobileUI.tsx
@@ -24,21 +24,15 @@ export const MobileUI: React.FC<MobileUIProps> = ({ className, outputImage, hand
   };
 
   return (
-    <div className="flex flex-col items-center mx-auto w-full bg-slate-600 max-w-[480px]">
+    <div className="flex flex-col items-center mx-auto w-full bg-slate-600 font-serif max-w-[480px]">
       <div className="flex gap-0 justify-center self-stretch px-5">
-        <div className="flex gap-1 justify-center px-11 py-6 text-lg tracking-tight leading-3 text-center text-black whitespace-nowrap font-[590]">
-          
-        </div>
-        
-        <div className="flex gap-2.5 justify-center px-7 py-6">
-         
-        </div>
+        <div className="flex gap-1 justify-center px-11 py-6 text-lg tracking-tight leading-3 text-center text-black whitespace-nowrap font-[590]"></div>
       </div>
       <div className="flex gap-0 mt-2 text-base tracking-tight leading-4 text-center whitespace-nowrap">
         <div className="flex flex-col grow shrink-0 basis-0 w-fit">
           <div className="flex gap-4 px-9 py-5 rounded-2xl bg-slate-500">
             <div className="flex flex-col flex-1 text-white">
-              <div className="font-bold">Upload Image</div>
+              <div className="self-start font-bold">1. Upload Image</div>
               <input
                 type="file"
                 accept="image/*"
@@ -58,32 +52,32 @@ export const MobileUI: React.FC<MobileUIProps> = ({ className, outputImage, hand
             </button>
           </div>
           <div className="flex flex-col px-3 py-6 mt-3 text-white rounded-2xl bg-slate-500">
-            <div className="self-start ml-6 font-bold">Adjust settings</div>
-            <div className="flex justify-center items-center px-16 py-5 mt-5 rounded-2xl bg-slate-400">
-              <div className="flex flex-col max-w-full w-[108px]">
-                <div className="self-end">Intensity</div>
-                <div className="container mx-auto px-4">
-                  <input type="range" className="w-4/5 bg-white h-2 rounded-lg appearance-none cursor-pointer" />
-                </div>
+            <div className="self-start ml-6 font-bold">2. Adjust settings</div>
+            <div className="flex flex-col justify-center items-center px-10 py-5 mt-4 rounded-2xl bg-slate-400">
+              <div className="pb-2 flex flex-col max-w-full w-[106px]">
+                <div>Intensity</div>
               </div>
+              <div className="container mx-2">
+                  <input type="range" className="w-full bg-white h-1 rounded-lg appearance-none cursor-pointer" />
+                </div>
             </div>
-            <div className="flex flex-col justify-center items-center px-16 py-5 mt-4 rounded-2xl bg-slate-400">
-              <div className="flex flex-col max-w-full w-[106px]">
+            <div className="flex flex-col justify-center items-center px-10 py-5 mt-4 rounded-2xl bg-slate-400">
+              <div className="pb-2 flex flex-col max-w-full w-[106px]">
                 <div>Render Quality</div>
               </div>
-              <div className="container mx-auto px-4">
-                  <input type="range" className="w-4/5 bg-white h-2 rounded-lg appearance-none cursor-pointer" />
+              <div className="container mx-2">
+                  <input type="range" className="w-full bg-white h-1 rounded-lg appearance-none cursor-pointer" />
                 </div>
             </div>
           </div>
         </div>
-        <div className="shrink-0 self-start mt-5 rounded border border-solid bg-slate-600 bg-opacity-50 border-white border-opacity-50 h-[275px] w-[9px]" />
       </div>
-      <div className="items-center px-16 pt-3.5 pb-8 mt-4 w-full text-base tracking-tight leading-4 text-center text-white whitespace-nowrap rounded-2xl bg-slate-400 max-w-[359px]">
+      {/* <div className="shrink-0 self-end mt-5 rounded border border-solid bg-slate-600 bg-opacity-50 border-white border-opacity-50 h-[275px] w-[9px]" /> */}
+
+      <div className="items-center px-16 pt-3.5 pb-3.5 mt-4 w-full text-base tracking-tight leading-4 text-center text-white whitespace-nowrap rounded-t-2xl bg-slate-500 max-w-[367px]">
         Resulting image:
       </div>
-      <div className="flex z-10 flex-col items-center px-16 pt-20 pb-2 mt-0 w-full bg-slate-500 max-w-[359px]">
-        <div className="shrink-0 mt-56 bg-black rounded-md h-[5px] w-[140px]" />
+      <div className="flex z-10 flex-col items-center px-16 pt-20 pb-2 mt-0 w-full bg-slate-400 max-w-[367px] overflow-auto">
       </div>
     </div>
   );
