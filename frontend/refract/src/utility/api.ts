380fc59a75d25a6d4db74dafaa52fe633d527a88
Jesus Velarde <jev@umail.ucsb.edu>
2024-04-29 17:52:50 -0700

    api code update

--- frontend/refract/src/utility/api.ts
+++ frontend/refract/src/utility/api.ts
@@ -1,4 +1,21 @@
 
+export interface JobStatus {
+    job_id: number;
+    user_id: string;
+    input_url: string;
+    alpha: number;
+    p_allow: number;
+    compress_size: number;
+    status: string;
+    ordered_ts: number;
+    processed_ts?: number;
+    completed_ts?: number;
+    output_url?: string;
+};
+
+const s3BucketURL: string = process.env.S3_BUCKET_URL || '';
+const apiEndpoint: string = process.env.API_ENDPOINT || '';
+
 /**
  * 
  * @param imageFile - The image file to upload
@@ -7,43 +24,143 @@
  * Will throw an error if the API endpoint is not found in the environment variables or 
  * if the fetch request fails
  */
-export async function runModel(imageFile: File, auth_token: string): Promise<string | void> {
-    const apiEndpoint: string = process.env.API_ENDPOINT || '';
+
+
+export async function queueModel(
+    input_url: string, 
+    compress_size: number, 
+    p_allow: number, 
+    alpha: number
+): Promise<number | void> {
+    
     if (apiEndpoint === '') {
-        throw new Error('API endpoint not found in environment variables');
+        throw new Error('API_ENDPOINT not found in environment variables');
+    }
+
+    if (s3BucketURL === '') {
+        throw new Error('S3_BUCKET_URL not found in environment variables');
     }
     
-    const url = new URL(`${apiEndpoint}/queue-model`);
+    const url = new URL(`${apiEndpoint}/queue_refract_model`);
+    url.searchParams.append('input_url', input_url);
+    url.searchParams.append('compress_size', compress_size.toString());
+    url.searchParams.append('p_allow', p_allow.toString());
+    url.searchParams.append('alpha', alpha.toString());
+
     const method = 'POST';
     const headers = {
-        'Content-Type': 'multipart/form-data',
-        'Authorization': `Bearer ${auth_token}`,
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${auth_token}`,
     };
-
-
-    const formData = new FormData();
-    formData.append('image', imageFile);
-
+    
+    
     const options = {
         method,
-        headers,
-        body: formData,
+        headers
     };
-
+    
     try {
         const response = await fetch(url.toString(), options);
         if (response.ok) {
             const data = response.json();
             data.then((json) => {
-                return json['imageUrl'];
-            }
-            ).catch((error) => {
+                return json['job_id'];
+            }).catch((error) => {
                 throw error;
             }
-            );
+        );
+    }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error uploading image');
+    }
+}
+
+export async function getUploadLinkURL(ext: string): Promise< {url: string, filename: string} | void > {
+    if (apiEndpoint === '') {
+        throw new Error('API_ENDPOINT not found in environment variables');
+    }
+
+    const url = new URL(`${apiEndpoint}/refract_get_upload_link`);
+    url.searchParams.append('extension', ext);
+    const method = 'GET';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    
+    const options = {
+        method,
+        headers,
+    };
+    
+    try {
+        const response = await fetch(url.toString(), options);
+        if (response.ok) {
+            const data = response.json();
+            data.then((json) => {
+                return json;
+            }).catch((error) => {
+                throw error;
+            }
+        );
+    }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error getting upload link');
+    }
+}
+
+export async function uploadImage(imageFile: File, uploadLink: string): Promise<string | void> {
+    const formData = new FormData();
+    formData.append('file', imageFile);
+
+    const options = {
+        method: 'PUT',
+        body: formData,
+    };
+
+    try {
+        const response = await fetch(uploadLink, options);
+        if (response.ok) {
+            return uploadLink;
         }
     } catch (error) {
         console.error(error);
         throw new Error('Error uploading image');
+    }
+}
+
+export async function getJobStatus(job_id: number): Promise<JobStatus | void> {
+    if (apiEndpoint === '') {
+        throw new Error('API_ENDPOINT not found in environment variables');
+    }
+
+    const url = new URL(`${apiEndpoint}/refract_get_job_status`);
+    url.searchParams.append('job_id', job_id.toString());
+    const method = 'GET';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    
+    const options = {
+        method,
+        headers,
+    };
+    
+    try {
+        const response = await fetch(url.toString(), options);
+        if (response.ok) {
+            const data = response.json();
+            data.then((json) => {
+                return json;
+            }
+            ).catch((error) => {
+                throw error;
+            }
+        );
+    }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error getting job status');
     }
 }
